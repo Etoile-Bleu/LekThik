d91933406fcdb679aabc1aeeb4afad951541ba94
DELBARRE Matheo <tarto6351@gmail.com>
2026-09-24 23:24:11 +0200

    refactor(domain): export find_by_id and drop confirmation error types

--- backend/domain/src/lib.rs
+++ backend/domain/src/lib.rs
@@ -2,7 +2,7 @@ mod session;
 mod user;
 
 pub use session::{TokenError, TokenIssuer};
-pub use user::{ConfirmUserError, CreateUserError, NewUser, User, UserRepo, UserRepoError};
+pub use user::{CreateUserError, NewUser, User, UserRepo, UserRepoError};
 
 #[cfg(feature = "test-utils")]
 pub use user::MockUserRepo;
